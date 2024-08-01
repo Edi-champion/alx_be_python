7d1a2130061a46e38ec8111a465198203f2207db
Edi-champion <estherafi0406@gmail.com>
2024-08-01 05:39:17 -0700

    multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+number =int((input("Enter a number to see the multiplication table: "))
+
+for i in range(1, 11) :
+    multiplication = number * i
+    print("{} * {} = {}".format(number, i, multiplication)) 
