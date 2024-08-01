ebd74e6372db2d16e4b01337af63ad529757991a
Edi-champion <estherafi0406@gmail.com>
2024-08-01 04:56:51 -0700

    operations

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
 
 
-# match_case_calculator.py
+# match_case_calculator
 
-def get_number(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
-def main():
-    # Prompt for user input
-    num1 = get_number("Enter the first number: ")
-    num2 = get_number("Enter the second number: ")
-    operation = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-    # Perform the calculation using match case
-    match operation:
-        case '+':
-            result = num1 + num2
-            print(f"The result is {result}")
-        case '-':
-            result = num1 - num2
-            print(f"The result is {result}")
-        case '*':
-            result = num1 * num2
-            print(f"The result is {result}")
-        case '/':
-            if num2 != 0:
-                result = num1 / num2
-                print(f"The result is {result}")
-            else:
-                print("Cannot divide by zero.")
-        case _:
-            print("Invalid operation. Please choose one of +, -, *, /.")
+match operation :
+    case "+" :
+        summ = num1 + num2
+        print("{} + {} = {}" .format(num1, num2, summ))
+    case "-" :
+        dif = num1 - num2 
+        print("{} - {} = {}".format(num1, num2, dif))
+    case "*" :
+        product = num1 * num2
+        print ("{} * {} = {}" .format(num1, num2, product))
+    case "/": 
+        if num2 == 0:
+            print ("Cannot divide by 0")
+        else :
+            div = num1 / num2 :
 
-if __name__ == "__main__":
-    main()
-
+            print ("{} / {} = {}" .format(num1, num2, div))
+    case_:
+        print("Invalid operation")
