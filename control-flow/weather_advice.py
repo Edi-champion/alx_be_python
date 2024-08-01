fa595b75851152efef37752a8394aa96f6f03e58
Edi-champion <estherafi0406@gmail.com>
2024-08-01 06:22:33 -0700

    syntax corrected

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,10 +3,10 @@
 
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather =="sunny" :
+if weather =="sunny":
     print("Wear a t-shirt and sunglasses.")
 
-elif weather =="rainy" :
+elif weather =="rainy":
     print("Don't forget your umbrella and a raincoat.")
 
 elif weather =="cold":
